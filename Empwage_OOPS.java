79bcc3260b6866ed9bf4d5b60392e33cc1c8f057
Nilesh Shinde <nileshshindes98@gmail.com>
2023-06-24 21:56:54 +0530

    Added : UC4 Branch Switch case

--- Empwage_OOPS.java
+++ Empwage_OOPS.java
@@ -7,6 +7,7 @@ public class Empwage_OOPS {
     public static int totalWorkingDay;
     public static double totalWage;
 
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program using OOPS concept");
         Attendance uc1 = new Attendance();
@@ -25,14 +26,18 @@ public class Empwage_OOPS {
     }
 
     public static class Attendance extends Empwage_OOPS {
+        int empHrs = 0;
+
         public void checkAttendance() {
             double checkEmpAtt = (int) (Math.floor(Math.random() * 10) % 4);
-            if (checkEmpAtt == FULL_TIME) {
-                System.out.println("FULL TIME Employee is Present ");
-            } else if (checkEmpAtt == PART_TIME) {
-                System.out.println("PART TIME Employee is Present ");
-            } else {
-                System.out.println("Employee is Absent ");
+            switch ((int) checkEmpAtt) {
+                case PART_TIME :
+                    empHrs = 4;
+                    break ;
+                case FULL_TIME :
+                    empHrs = 8;
+                default :
+                    empHrs = 0;
             }
         }
     }
